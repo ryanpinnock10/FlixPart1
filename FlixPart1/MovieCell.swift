73b17fc00ad2979ec3a0a1552071a9ed0036201c
ryanPinnock <ryanpinnock10@gmail.com>
2019-10-16 20:43:35 -0400

    Pod Alamofire Added

---
+++ FlixPart1/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  FlixPart1
+//
+//  Created by Ryan Pinnock on 10/16/19.
+//  Copyright © 2019 Ryan Pinnock. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
